5936667c00d5f9368318c9c6bd24a7f845e3ac00
unknown <Administrator@LAB187.tumo.lab>
2014-07-09 20:03:43 +0400

    footer

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -1,11 +1,17 @@
 <div class="clear"></div>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+<div class="footer">
+	<img src="<?=get_template_directory_uri();?>/images/logo.png" id="footerlogo">
+	<p id="copyright">©Copyright 2014.</p>
 </div>
+<div id="icons">
+       	<div id="facebook"><a href="https://www.facebook.com/pages/Chocolate/113472061996206?fref=ts" target="blank" class="icon"></a></div>
+       	<div id="twitter"><a href="https://twitter.com/ChocolateWeek" target="blank" class="icon"></a></div>
+       	<div id="flickr"><a href="http://www.flickr.com/search/?q=chocolate" target="blank" class="icon"></a></div>
+       	<div id="youtube"><a href="http://www.youtube.com/watch?v=0SCMs-xIu2U" target="blank" class="icon"></a></div>
+       </div>
 </footer>
 </div>
-<?php wp_footer(); ?>
 </body>
 </html>
