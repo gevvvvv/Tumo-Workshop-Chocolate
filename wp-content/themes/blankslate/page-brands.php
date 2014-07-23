d75c9c8e2941fec60d7f796c20e86466790ad535
unknown <Administrator@LAB416.tumo.lab>
2014-07-23 20:51:22 +0400

    Vsyo

--- wp-content/themes/blankslate/page-brands.php
+++ wp-content/themes/blankslate/page-brands.php
@@ -18,26 +18,20 @@ $items = $items->posts;
 <div class="brands-text">
 	<h1>Brands</h1>
 </div>
+<div id="brands-content">
 <?php foreach($items as $item):
     $item->meta = get_fields($item->ID);
 ?>
-	<div class="grid_3 expand">
+    <div class="grid_3 expand">
 		<div class="logoprice">
         	<img class="rotate" alt="" src="<?=$item->meta["image"]["url"];?>"> <!--you can edit the image, open the psd file for the vector image format-->
         </div>
         <div class="price">
-        	<p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p> <!--you can edit-->
-       		<ul>
-            	<li><p>Lorem ipsum dolor sit amet consec</p></li> <!--you can edit-->
-                <li><p>Lorem ipsum dolor sit amet consec</p></li> <!--you can edit-->
-                <li><p class="noborder">Lorem ipsum dolor sit amet consec</p></li> <!--you can edit-->
-            </ul>
-            <p class="btn red"><a href="#">Cupcakes</a></p> <!--you can edit-->
+        	<p><?=$item->meta["desc"];?></p>
         </div>
         <div class="triangle"></div>
     </div>
 <?php endforeach; ?>
+</div>
 
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
