fefb6df1a69147710fae811ebeea408766dc648c
unknown <Administrator@LAB703.tumo.lab>
2014-07-23 18:54:02 +0400

    contact

--- wp-content/themes/blankslate/page-contact.php
+++ wp-content/themes/blankslate/page-contact.php
@@ -13,6 +13,39 @@
 			<?php the_content(); ?>
 			<div class="entry-links"><?php wp_link_pages(); ?></div>
 			</section>
+			<div class="contactImages">
+				<div class="pic-container">
+				    <img src="/wp-content/uploads/2014/07/10171831_446235185511121_2462388784950046695_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10171831_446235185511121_2462388784950046695_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10171831_446235185511121_2462388784950046695_n.jpg" class="pic" />
+				    <a class="pic-caption" href="#!">Hi, i'm Rikarsen</a>
+				</div>
+			</div>
+			<div class="contactImages">
+				<div class="pic-container pic-container2">
+				    <img src="/wp-content/uploads/2014/07/10567316_609934855789335_1680734030_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10567316_609934855789335_1680734030_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10567316_609934855789335_1680734030_n.jpg" class="pic" />
+				    <a class="pic-caption pic-caption2" href="#!">Hi, i'm Mary</a>
+				</div>
+			</div>
+			<div class="contactImages">
+				<div class="pic-container">
+				    <img src="/wp-content/uploads/2014/07/10563443_609934852456002_1974613512_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10563443_609934852456002_1974613512_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10563443_609934852456002_1974613512_n.jpg" class="pic" />
+				    <a class="pic-caption" href="#!">Hi, i'm Rafik</a>
+				</div>
+			</div>
+			<div class="contactImages">
+				<div class="pic-container pic-container2">
+				    <img src="/wp-content/uploads/2014/07/10524731_609933295789491_7217276025320766692_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10524731_609933295789491_7217276025320766692_n.jpg" class="pic" />
+				    <img src="/wp-content/uploads/2014/07/10524731_609933295789491_7217276025320766692_n.jpg" class="pic" />
+				    <a class="pic-caption pic-caption2" href="#!">Hi, i'm Davit</a>
+				</div>
+			</div>
+			<div class="clear"></div>
 		</div>
 	</div>
 </article>
